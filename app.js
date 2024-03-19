607f9fdcbd56fb4a632de0d5c1817268f2f3c6c6
Abakar Anas <info.abakaranas@gmail.com>
2024-03-19 00:53:48 +0000

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-// npm - global command, comes with code
-// npm --version
+const http = require('http');
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo install -g <packageName> (max)
+const server = http.createServer((req,res)=>{
+  if(req.url === '/'){
+    res.end('Home Page')
+  }else if (req.url === '/about'){
+    // BLOCKING CODE!
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++){
+        console.log(`${i} ${j}`);
+      }
+      
+    }
+    res.end('About page')
+  }else {
+    res.end('The page you are looking for doesn\'t exist')
+  }
+})
 
-// package.json - manifest file 
-// manual approach (create package.json in the root)
-// npm init (step by step, enter to skip)
-// npm init -y (everythung default)
-
-const _ = require('lodash');
-
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+server.listen(5001,() => {
+  console.log(('Server is listening in the port 5001'));
+})
